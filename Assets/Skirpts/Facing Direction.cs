8a3ebd01eac69fd0b97c427bcce0829f956b701d
Jason <jason974448@gmail.com>
2024-11-02 16:31:38 -0300

    Got the lil guy to actually spin in the editor

--- Assets/Skirpts/Facing Direction.cs
+++ Assets/Skirpts/Facing Direction.cs
@@ -14,12 +14,15 @@ public class FacingDirection : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(Vector3.zero, (OtherObject.transform.position  - transform.position).normalized);
+        Gizmos.DrawLine(Vector3.zero, (OtherObject.transform.position  - 
+            transform.position).normalized);
 
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(Vector3.zero, (transform.position - OtherObject.transform.position).normalized);
+        Gizmos.DrawLine(Vector3.zero, (transform.position - 
+            OtherObject.transform.position).normalized);
 
-        Vector3 directionTo = (transform.position - OtherObject.transform.position);
+        Vector3 directionTo = (transform.position - 
+            OtherObject.transform.position).normalized;
         OtherObject.transform.forward = directionTo;
     }
 
