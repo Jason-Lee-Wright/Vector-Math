e8c55007f1d3d55fe580e704439e854fbd0cee92
Jason <jason974448@gmail.com>
2024-11-02 20:28:30 -0300

    Added comments to code

--- Assets/Skirpts/Facing Direction.cs
+++ Assets/Skirpts/Facing Direction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FacingDirection : MonoBehaviour
 {
     public GameObject OtherObject;
+    public int distance;
 
     private void Start()
     {
@@ -13,14 +14,29 @@ public class FacingDirection : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        //Makes a yellow line facing away from the cube
+        //shortens the line using the .normalized
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(Vector3.zero, (OtherObject.transform.position  - 
             transform.position).normalized);
 
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(Vector3.zero, (transform.position - 
-            OtherObject.transform.position).normalized);
 
+        //Changes the color based off the distance between objects.
+        if (Vector3.Distance(transform.position, OtherObject.transform.position)
+            > distance)
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+        }
+
+        //Draws the green/red line.
+        Gizmos.DrawLine(Vector3.zero, (transform.position - 
+            OtherObject.transform.position));
+
+        //changes the rotation of the lil guy game object.
         Vector3 directionTo = (transform.position - 
             OtherObject.transform.position).normalized;
         OtherObject.transform.forward = directionTo;
